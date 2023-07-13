9dd16a34bd617bc2c67f3f174a98bd579d075036
CABlank <54187031+CABlank@users.noreply.github.com>
2023-07-14 01:13:15 +0800

    Summary

--- web/frontend/pages/BacklinkAnalysisPage.jsx
+++ web/frontend/pages/BacklinkAnalysisPage.jsx
@@ -8,44 +8,86 @@ import {
   Link,
   Text,
 } from "@shopify/polaris";
-import { TitleBar, useAppBridge } from "@shopify/app-bridge-react";
+import { TitleBar } from "@shopify/app-bridge-react";
 import { useTranslation, Trans } from "react-i18next";
+
 import { trophyImage } from "../assets";
+
 import { ProductsCard } from "../components";
-import { DomainExtractor } from "../components";
-import { getSessionToken } from "@shopify/app-bridge-utils";
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 export default function HomePage() {
-  const app = useAppBridge();
   const { t } = useTranslation();
-
-  const config = { apiKey: process.env.SHOPIFY_API_KEY, shopOrigin: params.shop};
-  const [data, setData] = useState(null);
-  const [storeUrl, setStoreUrl] = useState(null);
-
-  useEffect(() => {
-      getSessionToken(app)
-      .then((token) => {
-          axios.get(`https://${app.getState().shopOrigin}/admin/api/2023-07/shop.json`, {
-          headers: {
-              'X-Shopify-Access-Token': token,
-          }
-          })
-          .then(response => {
-              console.log('Shopify response:', response.data);
-              setStoreUrl(response.data.shop.myshopify_domain);
-          })
-          .catch(error => console.log(error));
-      });
-  }, []);
-
-  // You can replace this with your actual render code
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>Store URL: {storeUrl}</p>
-    </div>
+    <Page narrowWidth>
+      <TitleBar title={t("HomePage.title")} primaryAction={null} />
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <Stack
+              wrap={false}
+              spacing="extraTight"
+              distribution="trailing"
+              alignment="center"
+            >
+              <Stack.Item fill>
+                <TextContainer spacing="loose">
+                  <Text as="h2" variant="headingMd">
+                    {("HomePage.heading")}
+                  </Text>
+                  <p>
+                    <Trans
+                      i18nKey="HomePage.yourAppIsReadyToExplore"
+                      components={{
+                        PolarisLink: (
+                          <Link url="https://polaris.shopify.com/" external />
+                        ),
+                        AdminApiLink: (
+                          <Link
+                            url="https://shopify.dev/api/admin-graphql"
+                            external
+                          />
+                        ),
+                        AppBridgeLink: (
+                          <Link
+                            url="https://shopify.dev/apps/tools/app-bridge"
+                            external
+                          />
+                        ),
+                      }}
+                    />
+                  </p>
+                  <p>{t("HomePage.startPopulatingYourApp")}</p>
+                  <p>
+                    <Trans
+                      i18nKey="HomePage.learnMore"
+                      components={{
+                        ShopifyTutorialLink: (
+                          <Link
+                            url="https://shopify.dev/apps/getting-started/add-functionality"
+                            external
+                          />
+                        ),
+                      }}
+                    />
+                  </p>
+                </TextContainer>
+              </Stack.Item>
+              <Stack.Item>
+                <div style={{ padding: "0 20px" }}>
+                  <Image
+                    source={trophyImage}
+                    alt={t("HomePage.trophyAltText")}
+                    width={120}
+                  />
+                </div>
+              </Stack.Item>
+            </Stack>
+          </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <ProductsCard />
+        </Layout.Section>
+      </Layout>
+    </Page>
   );
 }
