b79e3679cf7a89274b265e7dc3e7680c8079d1e9
CABlank <54187031+CABlank@users.noreply.github.com>
2023-07-14 16:33:31 +0800

    summary

--- web/index.js
+++ web/index.js
@@ -1,6 +1,6 @@
 // @ts-check
 import { join } from "path";
-import { readFileSync } from "fs";
+import { readFileSync, existsSync } from "fs";
 import express from "express";
 import cors from "cors";
 import serveStatic from "serve-static";
@@ -32,7 +32,6 @@ const connectDB = async () => {
 };
 connectDB();
 
-
  
 const PORTPROCC = process.env.PORT || 3001; // Changed to another port (3001) to avoid EADDRINUSE error
 
@@ -46,88 +45,26 @@ import { fileURLToPath } from "url";
 import https from "https";
 import react from "@vitejs/plugin-react";
 
-if (
-  process.env.npm_lifecycle_event === "build" &&
-  !process.env.CI &&
-  !process.env.SHOPIFY_API_KEY
-) {
-  console.warn(
-    "\nBuilding the frontend app without an API key. The frontend build will not run without an API key. Set the SHOPIFY_API_KEY environment variable when running the build command.\n"
-  );
-}
-
-const proxyOptions = {
-  target: `http://127.0.0.1:${BACKEND_PORT}`,
-  changeOrigin: false,
-  secure: true,
-  ws: false,
-};
-
-const host = process.env.HOST
-  ? process.env.HOST.replace(/https?:\/\//, "")
-  : "localhost";
-
-let hmrConfig;
-if (host === "localhost") {
-  hmrConfig = {
-    protocol: "ws",
-    host: "localhost",
-    port: 64999,
-    clientPort: 64999,
-  };
-} else {
-  hmrConfig = {
-    protocol: "wss",
-    host: host,
-    port: FRONTEND_PORT,
-    clientPort: 443,
-  };
-}
-
-export default defineConfig({
-  root: dirname(fileURLToPath(import.meta.url)),
-  plugins: [react()],
-  define: {
-    "process.env.SHOPIFY_API_KEY": JSON.stringify(process.env.SHOPIFY_API_KEY),
-  },
-  resolve: {
-    preserveSymlinks: true,
-  },
-  server: {
-    host: "localhost",
-    port: FRONTEND_PORT,
-    hmr: hmrConfig,
-    proxy: {
-      "^/(\\?.*)?$": proxyOptions,
-      "^/api(/|(\\?.*)?$)": proxyOptions,
-    },
-  },
-});
-
-
+// ...rest of your code...
 
 const STATIC_PATH =
   process.env.NODE_ENV === "production"
     ? `${process.cwd()}/dist` // Updated this line
     : `${process.cwd()}/frontend/`;
 
+// Add log to verify static path
+console.log(`Serving static files from ${STATIC_PATH}`);
+
+// Add log to verify index.html exists
+console.log(`Index file exists: ${existsSync(join(STATIC_PATH, 'index.html'))}`);
+
 const app = express();
 
 app.use(cors());
 
-// Set up Shopify authentication and webhook handling
-app.get(shopify.config.auth.path, shopify.auth.begin());
-app.get(
-  shopify.config.auth.callbackPath,
-  shopify.auth.callback(),
-  shopify.redirectToShopifyOrAppRoot()
-);
-app.post(
-  shopify.config.webhooks.path,
-  shopify.processWebhooks({ webhookHandlers: GDPRWebhookHandlers })
-);
+// ...rest of your code...
 
-app.use('/api', apiRoutes);
+app.use(serveStatic(STATIC_PATH, { index: false }));
 
 // If you are adding routes outside of the /api path, remember to
 // also add a proxy rule for them in web/frontend/vite.config.js
@@ -136,30 +73,7 @@ app.use("/api/*", shopify.validateAuthenticatedSession());
 
 app.use(express.json());
 
-app.get("/api/products/count", async (_req, res) => {
-  const countData = await shopify.api.rest.Product.count({
-    session: res.locals.shopify.session,
-  });
-  res.status(200).send(countData);
-});
-
-app.get("/api/products/create", async (_req, res) => {
-  let status = 200;
-  let error = null;
-
-  try {
-    await productCreator(res.locals.shopify.session);
-  } catch (e) {
-    console.log(`Failed to process products/create: ${e.message}`);
-    status = 500;
-    error = e.message;
-  }
-  res.status(status).send({ success: status === 200, error });
-});
-
-app.use(shopify.cspHeaders());
-
-app.use(serveStatic(STATIC_PATH, { index: false }));
+// ...rest of your code...
 
 app.use("/*", shopify.ensureInstalledOnShop(), async (_req, res, _next) => {
   return res
