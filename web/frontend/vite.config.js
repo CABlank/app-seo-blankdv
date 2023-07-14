4c1d76b180245ea93cbbc45eefbe5570287e7b37
CABlank <54187031+CABlank@users.noreply.github.com>
2023-07-14 17:39:20 +0800

    Update vite.config.js

--- web/frontend/vite.config.js
+++ web/frontend/vite.config.js
@@ -2,15 +2,13 @@ import { defineConfig } from "vite";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
 
-// Load environment variables from .env file
-dotenv.config();
+const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY;
 
 if (
   process.env.npm_lifecycle_event === "build" &&
   !process.env.CI &&
-  !process.env.SHOPIFY_API_KEY
+  !SHOPIFY_API_KEY
 ) {
   console.warn(
     "\nBuilding the frontend app without an API key. The frontend build will not run without an API key. Set the SHOPIFY_API_KEY environment variable when running the build command.\n"
@@ -48,6 +46,9 @@ if (host === "localhost") {
 export default defineConfig({
   root: dirname(fileURLToPath(import.meta.url)),
   plugins: [react()],
+  define: {
+    "process.env.SHOPIFY_API_KEY": JSON.stringify(SHOPIFY_API_KEY),
+  },
   resolve: {
     preserveSymlinks: true,
   },
