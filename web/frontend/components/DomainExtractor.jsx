dd0f90bff0fb2a6ced86c8ab615615f6c199c159
CABlank <54187031+CABlank@users.noreply.github.com>
2023-07-18 21:59:19 +0800

    Update DomainExtractor.jsx

--- web/frontend/components/DomainExtractor.jsx
+++ web/frontend/components/DomainExtractor.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+/* import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { getSessionToken } from '@shopify/app-bridge-utils';
 import { useAppBridge } from '@shopify/app-bridge-react';
@@ -39,3 +39,4 @@ const DomainExtractor = () => {
 };
 
 export default DomainExtractor;
+*/
