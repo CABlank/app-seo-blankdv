5c73bfbfb219b9110100f2a1b5e4d12ec2bf09d6
CABlank <54187031+CABlank@users.noreply.github.com>
2023-07-18 22:01:24 +0800

    Update index.js

--- web/index.js
+++ web/index.js
@@ -1,35 +1,12 @@
 // @ts-check
 import { join } from "path";
-import { readFileSync, existsSync } from "fs";
+import { readFileSync } from "fs";
 import express from "express";
-import cors from "cors";
 import serveStatic from "serve-static";
-import mongoose from "mongoose"
-import apiRoutes from './routes/apiRoutes.js';
-import './jobs/updateData.js';
-
-import SeoData from './models/SeoData.js';
-import Session from './models/Session.js';
 
 import shopify from "./shopify.js";
 import productCreator from "./product-creator.js";
 import GDPRWebhookHandlers from "./gdpr.js";
-
-import dotenv from 'dotenv';
-dotenv.config();
-
-const MONGODB_CONNECTION_STRING = 'mongodb+srv://carlosblank333:9tQPbFvcUR369XIt@cluster0.0r2skgb.mongodb.net/test?retryWrites=true&w=majority'; 
-
-// Connecting to MongoDB
-const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('Database connected!');
-  } catch (err) {
-    console.log(err);
-  }
-};
-connectDB();
 
 const PORT = parseInt(
   process.env.BACKEND_PORT || process.env.PORT || "3000",
